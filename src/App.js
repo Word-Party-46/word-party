c921a78278a258e2bbcd0bc9efedc6fb0d702a0f
vincentYoung <vinni.xander@gmail.com>
2023-02-20 15:14:15 -0700

    set up app for secure routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 // modules
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Route, Routes } from "react-router-dom";
 // components
 import Footer from "./Components/Footer";
@@ -9,6 +9,11 @@ import NotFound from "./Components/NotFound";
 import SavedWords from "./Components/SavedWords";
 // local imports
 import "./App.css";
+import LogOut from "./Components/LogOut";
+import SignUp from "./Components/SignUp";
+import SignIn from "./Components/SignIn";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "./modules/firebase";
 
 const App = () => {
   // wordResultList is populated with searched word results from api
@@ -18,36 +23,70 @@ const App = () => {
   // state for adding/removing animation for adding/removing saved words
   const [savedWordIconToggleClassName, setSavedWordIconToggleClassName] =
     useState("");
+  const [loggedIn, setLoggedIn] = useState(null);
+  const [userId, setUserId] = useState(null);
+  const [userEmail, setUserEmail] = useState(null);
+
+  const authenticateUser = () => {
+    // Detects if user is already logged in
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setUserId(user.uid)
+        setUserEmail(user.email);
+      } else {
+        setUserId(null)
+        setUserEmail(null);
+      }
+    })
+  }
+
+  useEffect(() => {
+    authenticateUser();
+  }, [loggedIn])
 
   return (
     <div className="App">
       <Header savedWordIconToggleClassName={savedWordIconToggleClassName} />
-      <Routes>
-        <Route
-          path="/"
-          element={
-            // Includes form and results components
-            <Home
-              setApiIsLoading={setApiIsLoading}
-              setWordResultList={setWordResultList}
-              wordResultList={wordResultList}
-              apiIsLoading={apiIsLoading}
-              setSavedWordIconToggleClassName={setSavedWordIconToggleClassName}
-            />
-          }
-        />
-        <Route
-          path="/savedWords"
-          element={
-            <SavedWords
-              setWordResultList={setWordResultList}
-              setSavedWordIconToggleClassName={setSavedWordIconToggleClassName}
-            />
-          }
-        />
-        {/* route for incorrect URL */}
-        <Route path="*" element={<NotFound />} />
-      </Routes>
+      {
+        !(userId) ?
+          <>
+            <Routes>
+              <Route path='/' element={<SignIn />} />
+              <Route path='/SignUp' element={<SignUp setLoggedIn={setLoggedIn} />} />
+              {/* route for incorrect URL */}
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </> :
+          <>
+            <LogOut userEmail={userEmail} setLoggedIn={setLoggedIn} />
+            <Routes>
+              <Route
+                path="/"
+                element={
+                  // Includes form and results components
+                  <Home
+                    setApiIsLoading={setApiIsLoading}
+                    setWordResultList={setWordResultList}
+                    wordResultList={wordResultList}
+                    apiIsLoading={apiIsLoading}
+                    setSavedWordIconToggleClassName={setSavedWordIconToggleClassName}
+                  />
+                }
+              />
+              <Route
+                path="/savedWords"
+                element={
+                  <SavedWords
+                    setWordResultList={setWordResultList}
+                    setSavedWordIconToggleClassName={setSavedWordIconToggleClassName}
+                  />
+                }
+              />
+              {/* route for incorrect URL */}
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </>
+      }
       <Footer />
     </div>
   );
--- src/modules/firebase.js
+++ src/modules/firebase.js
@@ -1,4 +1,5 @@
 import { initializeApp } from "firebase/app";
+import { getAuth } from 'firebase/auth'
 
 const firebaseConfig = {
   apiKey: "AIzaSyBugIf6J5ajB6PwwWHjEw9slK7RENSu3nU",
@@ -10,4 +11,5 @@ const firebaseConfig = {
 };
 
 const firebase = initializeApp(firebaseConfig);
+export const auth = getAuth(firebase)
 export default firebase;
