1328a16256acec0bcfffe2ec9d669f2b3a31e1f8
vincentYoung <vinni.xander@gmail.com>
2023-02-20 15:15:24 -0700

    implement logout functionality

---
+++ src/Components/LogOut.js
@@ -0,0 +1,32 @@
+import { onAuthStateChanged, signOut } from 'firebase/auth';
+import { useEffect, useState } from 'react'
+import { auth } from '../modules/firebase'
+import { useNavigate } from "react-router-dom";
+import { AiOutlineLogout } from "react-icons/ai"
+import throwAlert from '../modules/alerts';
+// components
+
+const LogOut = ({ userEmail, setLoggedIn, setWordList }) => {
+
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    signOut(auth)
+      .then(() => {
+        setLoggedIn(false)
+        navigate("/");
+      })
+      .catch((error) => {
+        throwAlert(error.message)
+      });
+  }
+
+  return (
+    <div className='log-out'>
+      <p>Signed In as {userEmail}</p>
+      <button onClick={handleClick}><AiOutlineLogout /></button>
+    </div>
+  )
+}
+
+export default LogOut;
